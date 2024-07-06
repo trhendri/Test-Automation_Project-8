87c1cecd8be9dd92d4f71792e14100318a873aef
Talise Hendrix <trhendri1@gmail.com>
2024-07-06 02:36:24 -0500

    test2

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -21,4 +21,18 @@ describe('Create an order', () => {
     })
 })
 
-test
+/*
+Tests should cover the following:
+
+Setting the address
+Selecting Supportive plan
+Filling in the phone number
+Adding a credit card (Tip: the “link” button doesn’t become active until the card CVV field on the “Adding a card” modal id=”code” class=”card-input” loses focus. To change focus you can simulate the user pressing TAB or clicking somewhere else on the screen).
+Writing a message for the driver
+Ordering a Blanket and handkerchiefs (Tip: there are two selectors to be aware of here. One selector to click on and one to run expect on to verify that the state changed).
+Ordering 2 Ice creams
+The car search modal appears
+Waiting for the driver info to appear in the modal (optional) In addition to the steps above there is an optional step you can check. This one is a bit more tricky than the others but it’s good practice since you will likely encounter more difficult tasks in your career.
+
+
+*/
